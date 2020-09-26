f50923878db1724317c8c884f8b477cb07e89830
EthanMac <EthanMac@gmail.com>
2020-09-26 22:12:04 +0800

    update

---
+++ disp_dt_feature_standing.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from s_knn_dtw import KnnDtw
+plt.style.use('bmh')
+import s_data_loader as data_loader
+
+
+dt = data_loader.load_feature()
+# dt = data_loader.load_feature_time()
+# dt = data_loader.load_feature_freq()
+# dt = data_loader.load_raw_acc_x()
+# dt = data_loader.load_raw_acc_z()
+
+# Mapping table for classes
+labels = dt.labels
+x_train = dt.x_train
+y_train = dt.y_train
+x_test = dt.x_test
+y_test = dt.y_test
+
+plt.figure(figsize=(11,7))
+colors = ['#D62728','#2C9F2C','#FD7F23','#1F77B4','#9467BD',
+          '#8C564A','#7F7F7F','#1FBECF','#E377C2','#BCBD27']
+
+for i, r in enumerate([0,1,2,3,4,5]):
+    plt.subplot(3,2,i+1)
+    plt.plot(x_train[r], label=labels[y_train[r]]+str(r), color=colors[i], linewidth=2)
+    plt.xlabel('Samples @50Hz')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+from s_py_shell import is_in_ipython
+if is_in_ipython():
+    plt.show()
--- s_data_loader.py
+++ s_data_loader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class data(object):
     def __init__(self):
@@ -19,9 +20,18 @@ def load_raw_acc_x():
 def load_raw_acc_z():
     return _load("raw_acc_z")
 
+def load_raw_acc_o():
+    return _load("raw_acc_o")
+
 def _load(dt_type):
     print("load data...{}".format(dt_type))
     dt = data()
+
+    # Create empty lists
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
 
     # Import the HAR dataset
     if dt_type == "feature" or dt_type == "feature_time" or dt_type == "feature_freq":
@@ -42,72 +52,97 @@ def _load(dt_type):
         x_test_file = open('data/UCI-HAR-Dataset/test/InertialSignals/body_acc_z_test.txt', 'r')
         y_test_file = open('data/UCI-HAR-Dataset/test/y_test.txt', 'r')
 
-
-    # Create empty lists
-    x_train = []
-    y_train = []
-    x_test = []
-    y_test = []
-
-    # Mapping table for classes
-    labels = {1:'WALKING', 2:'WALKING UPSTAIRS', 3:'WALKING DOWNSTAIRS',
-            4:'SITTING', 5:'STANDING', 6:'LAYING'}
+    elif dt_type == "raw_acc_o":
+        x_train_file_0 = open('data/UCI-HAR-Dataset/train/InertialSignals/body_acc_x_train.txt', 'r')
+        x_train_file_1 = open('data/UCI-HAR-Dataset/train/InertialSignals/body_acc_y_train.txt', 'r')
+        x_train_file_2 = open('data/UCI-HAR-Dataset/train/InertialSignals/body_acc_z_train.txt', 'r')
+        y_train_file = open('data/UCI-HAR-Dataset/train/y_train.txt', 'r')
+        x_test_file_0 = open('data/UCI-HAR-Dataset/test/InertialSignals/body_acc_x_test.txt', 'r')
+        x_test_file_1 = open('data/UCI-HAR-Dataset/test/InertialSignals/body_acc_y_test.txt', 'r')
+        x_test_file_2 = open('data/UCI-HAR-Dataset/test/InertialSignals/body_acc_z_test.txt', 'r')
+        y_test_file = open('data/UCI-HAR-Dataset/test/y_test.txt', 'r')      
 
     # Loop through datasets
-    for x in x_train_file:
-        tmp = [float(ts) for ts in x.split()]
-        if dt_type == "feature_time":
-            x_train.append(tmp[0:265])
-        elif dt_type == "feature_freq":
-            x_train.append(tmp[266:])
-        else:
-            x_train.append(tmp)
+        
 
     for y in y_train_file:
         y_train.append(int(y.rstrip('\n')))
-        
-    for x in x_test_file:
-        #x_test.append([float(ts) for ts in x.split()])
-        tmp = [float(ts) for ts in x.split()]
-        if dt_type == "feature_time":
-            x_test.append(tmp[0:265])
-        elif dt_type == "feature_freq":
-            x_test.append(tmp[266:])
-        else:
-            x_test.append(tmp)
-        
     for y in y_test_file:
         y_test.append(int(y.rstrip('\n')))
-        
+    y_test = np.array(y_test)
+    y_train = np.array(y_train)
+
+    if dt_type == "raw_acc_o":
+        tmp0 = []
+        tmp1 = []
+        tmp2 = []
+        for x in x_train_file_0:
+            t0 = [float(ts) for ts in x.split()]
+            tmp0.append(t0)
+        for x in x_train_file_1:
+            t1 = [float(ts) for ts in x.split()]
+            tmp1.append(t1)        
+        for x in x_train_file_2:
+            t2 = [float(ts) for ts in x.split()]
+            tmp2.append(t2)        
+        for i in range(0, len(tmp0)):
+            tlo = [] 
+            tl = tmp0[i]
+            for j in range(0, len(tl)):
+                tlo.append(math.sqrt(tmp0[i][j]*tmp0[i][j] + tmp1[i][j]*tmp1[i][j] + tmp2[i][j]*tmp2[i][j]))
+            x_train.append(tlo)
+
+        tmp0 = []
+        tmp1 = []
+        tmp2 = []
+        for x in x_test_file_0:
+            t0 = [float(ts) for ts in x.split()]
+            tmp0.append(t0)
+        for x in x_test_file_1:
+            t1 = [float(ts) for ts in x.split()]
+            tmp1.append(t1)        
+        for x in x_test_file_2:
+            t2 = [float(ts) for ts in x.split()]
+            tmp2.append(t2)        
+        for i in range(0, len(tmp0)):
+            tlo = [] 
+            tl = tmp0[i]
+            for j in range(0, len(tl)):
+                tlo.append(math.sqrt(tmp0[i][j]*tmp0[i][j] + tmp1[i][j]*tmp1[i][j] + tmp2[i][j]*tmp2[i][j]))
+            x_test.append(tlo)  
+    else:
+        for x in x_train_file:
+            tmp = [float(ts) for ts in x.split()]
+            if dt_type == "feature_time":
+                x_train.append(tmp[0:265])
+            elif dt_type == "feature_freq":
+                x_train.append(tmp[266:])
+            else:
+                x_train.append(tmp)
+        for x in x_test_file:
+            #x_test.append([float(ts) for ts in x.split()])
+            tmp = [float(ts) for ts in x.split()]
+            if dt_type == "feature_time":
+                x_test.append(tmp[0:265])
+            elif dt_type == "feature_freq":
+                x_test.append(tmp[266:])
+            else:
+                x_test.append(tmp)
     # Convert to numpy for efficiency
     x_train = np.array(x_train)
-    y_train = np.array(y_train)
     x_test = np.array(x_test)
-    y_test = np.array(y_test)
 
     print("load data done.")
-    if dt_type == "raw_acc_x" or dt_type == "raw_acc_z":
+    if dt_type == "raw_acc_x" or dt_type == "raw_acc_z" or dt_type == "raw_acc_o":
         print("normalize data")
         for i in range(0, len(x_train)):
             x_train[i] = x_train[i] / np.linalg.norm(x_train[i])
         for i in range(0, len(x_test)):
             x_test[i] = x_test[i] / np.linalg.norm(x_test[i])
-    # elif dt_type == "feature_time":
-    #     print("crop time only")
-    #     for i in range(0, len(x_train)):
-    #         tmp = x_train[i][0:265]
-    #         x_train[i] = tmp 
-    #     for i in range(0, len(x_test)):
-    #         tmp = x_test[i][0:265]
-    #         x_test[i] = tmp
-    # elif dt_type == "feature_freq":
-    #     print("normalicrop freq")
-    #     for i in range(0, len(x_train)):
-    #         tmp = x_train[i][266:]
-    #         x_train[i]  = tmp
-    #     for i in range(0, len(x_test)):
-    #         tmp  = x_test[i][266:]
-    #         x_test[i] = tmp
+
+    # Mapping table for classes
+    labels = {1:'WALKING', 2:'WALKING UPSTAIRS', 3:'WALKING DOWNSTAIRS',
+            4:'SITTING', 5:'STANDING', 6:'LAYING'}
 
     dt.x_train = x_train
     dt.y_train = y_train
