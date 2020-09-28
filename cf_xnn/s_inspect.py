4f3c9ec1c198eb626126001905894a2e2835a63b
Ethan <ethan1.jiang@gmail.com>
2020-09-28 14:38:24 +0800

    ck and sl -- the more training data, the better the match

--- cf_xnn/s_inspect.py
+++ cf_xnn/s_inspect.py
@@ -1,5 +1,11 @@
 def inspect_xnn(model):
+    import numpy as np
     print(model)
     print("layers: {}".format(len(model.coefs_)))
+    total_co = 0
     for i in range(0, len(model.coefs_)):
-        print("layer {} hidden size: {}".format(i, len(model.coefs_[i])))    
+        layer = model.coefs_[i]
+        print("layer {} hidden size: {} shape: {}".format(i, len(layer), layer.shape))
+        total_co += np.size(layer)
+    print("total size of coefs_: {}".format(total_co))
+
