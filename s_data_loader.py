5a0ff08963d1deeef34f4c95b8abf9bc50297593
ethanmac <ethanmac@mac.com>
2020-09-29 23:18:20 +0800

    update s_data_loader

--- s_data_loader.py
+++ s_data_loader.py
@@ -2,9 +2,197 @@ import numpy as np
 import math
 import os
 
-class data(object):
-    def __init__(self):
-        pass
+
+def data_path(dat_file):
+    path = None
+    f1 = "data/UCI-HAR-Dataset/"
+    if os.path.isdir(f1):
+        os.path.isfile(f1 + dat_file)
+        path = os.path.abspath(f1 + dat_file)
+    if path is None: 
+        f2 = "../data/UCI-HAR-Dataset/"
+        if os.path.isdir(f2):
+            os.path.isfile(f1 + dat_file)
+            path = os.path.abspath(f2 + dat_file)
+    if path is None:
+        print("Failed find data file for " + dat_file)
+    else:
+        print("data file located {}".format(path))
+    return path  
+
+
+
+class DataHolder(object):
+    def __init__(self, x_train, y_train, x_test, y_test):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+
+        self.labels = {1:'WALKING', 2:'WALKING UPSTAIRS', 3:'WALKING DOWNSTAIRS',
+                        4:'SITTING', 5:'STANDING', 6:'LAYING'}
+
+
+class DataSrc(object):
+    def __init__(self, dt_type):
+        self.dt_type = dt_type
+        # Create empty lists
+        self.x_train = []
+        self.y_train = []
+        self.x_test = []
+        self.y_test = []
+
+    def load(self):
+        self._load_from_file()
+        self._transform()
+        self._normalize()
+        dh = DataHolder(self.x_train, self.y_train, self.x_test, self.y_test)
+        return dh
+
+
+    def _load_from_file(self):
+        # Import the HAR dataset
+        if self.dt_type == "feature" or self.dt_type == "feature_time" or self.dt_type == "feature_freq":
+            self.x_train_file = open(data_path('train/X_train.txt'), 'r')
+            self.y_train_file = open(data_path('train/y_train.txt'), 'r')
+            self.x_test_file = open(data_path('test/X_test.txt'), 'r')
+            self.y_test_file = open(data_path('test/y_test.txt'), 'r')
+     
+        elif self.dt_type == "raw_acc_x":
+            self.x_train_file = open(data_path('train/InertialSignals/body_acc_x_train.txt'), 'r')
+            self.y_train_file = open(data_path('train/y_train.txt'), 'r')
+            self.x_test_file = open(data_path('test/InertialSignals/body_acc_x_test.txt'), 'r')
+            self.y_test_file = open(data_path('test/y_test.txt'), 'r')
+
+        elif self.dt_type == "raw_acc_y":
+            self.x_train_file = open(data_path('train/InertialSignals/body_acc_y_train.txt'), 'r')
+            self.y_train_file = open(data_path('train/y_train.txt'), 'r')
+            self.x_test_file = open(data_path('test/InertialSignals/body_acc_y_test.txt'), 'r')
+            self.y_test_file = open(data_path('test/y_test.txt'), 'r')
+
+        elif self.dt_type == "raw_acc_z":
+            self.x_train_file = open(data_path('train/InertialSignals/body_acc_z_train.txt'), 'r')
+            self.y_train_file = open(data_path('train/y_train.txt'), 'r')
+            self.x_test_file = open(data_path('test/InertialSignals/body_acc_z_test.txt'), 'r')
+            self.y_test_file = open(data_path('test/y_test.txt'), 'r')
+
+        elif self.dt_type == "raw_acc_o":
+            self.x_train_file_0 = open(data_path('train/InertialSignals/body_acc_x_train.txt'), 'r')
+            self.x_train_file_1 = open(data_path('train/InertialSignals/body_acc_y_train.txt'), 'r')
+            self.x_train_file_2 = open(data_path('train/InertialSignals/body_acc_z_train.txt'), 'r')
+            self.y_train_file = open(data_path('train/y_train.txt'), 'r')
+            self.x_test_file_0 = open(data_path('test/InertialSignals/body_acc_x_test.txt'), 'r')
+            self.x_test_file_1 = open(data_path('test/InertialSignals/body_acc_y_test.txt'), 'r')
+            self.x_test_file_2 = open(data_path('test/InertialSignals/body_acc_z_test.txt'), 'r')
+            self.y_test_file = open(data_path('test/y_test.txt'), 'r')      
+
+        # Loop through datasets
+        for y in self.y_train_file:
+            self.y_train.append(int(y.rstrip('\n')))
+        for y in self.y_test_file:
+            self.y_test.append(int(y.rstrip('\n')))
+        self.y_test = np.array(self.y_test)
+        self.y_train = np.array(self.y_train)
+
+    def _transform(self):
+        if self.dt_type == "raw_acc_o":
+            self._transform_merge()
+        else:
+            self._transfrim_trim()
+
+        # Convert to numpy for efficiency
+        self.x_train = np.array(self.x_train)
+        self.x_test = np.array(self.x_test)
+
+    def _normalize(self):
+        if self.dt_type == "raw_acc_x" or self.dt_type == "raw_acc_y" or self.dt_type == "raw_acc_z" or self.dt_type == "raw_acc_o":
+            print("normalize data")
+            for i in range(0, len(x_train)):
+                self.x_train[i] = self.x_train[i] / np.linalg.norm(self.x_train[i])
+            for i in range(0, len(x_test)):
+                self.x_test[i] = self.x_test[i] / np.linalg.norm(self.x_test[i])
+
+
+    def _transform_merge(self):
+        if self.dt_type != "raw_acc_o":
+            return
+        
+        # merge or not
+        tmp0 = []
+        tmp1 = []
+        tmp2 = []
+        for x in self.x_train_file_0:
+            t0 = [float(ts) for ts in x.split()]
+            tmp0.append(t0)
+        for x in self.x_train_file_1:
+            t1 = [float(ts) for ts in x.split()]
+            tmp1.append(t1)        
+        for x in self.x_train_file_2:
+            t2 = [float(ts) for ts in x.split()]
+            tmp2.append(t2)        
+        for i in range(0, len(tmp0)):
+            tlo = [] 
+            tl = tmp0[i]
+            for j in range(0, len(tl)):
+                ox = math.sqrt(tmp0[i][j]*tmp0[i][j] + tmp1[i][j]*tmp1[i][j] + tmp2[i][j]*tmp2[i][j])
+                #if tmp2[i][j] < 0:
+                #    ox = -ox
+                tlo.append(ox)
+            self.x_train.append(tlo)
+
+        tmp0 = []
+        tmp1 = []
+        tmp2 = []
+        for x in self.x_test_file_0:
+            t0 = [float(ts) for ts in x.split()]
+            tmp0.append(t0)
+        for x in self.x_test_file_1:
+            t1 = [float(ts) for ts in x.split()]
+            tmp1.append(t1)        
+        for x in self.x_test_file_2:
+            t2 = [float(ts) for ts in x.split()]
+            tmp2.append(t2)        
+        for i in range(0, len(tmp0)):
+            tlo = [] 
+            tl = tmp0[i]
+            for j in range(0, len(tl)):
+                ox = math.sqrt(tmp0[i][j]*tmp0[i][j] + tmp1[i][j]*tmp1[i][j] + tmp2[i][j]*tmp2[i][j])
+                #if tmp2[i][j] < 0:
+                #    ox = -ox
+                tlo.append(ox)
+            x_test.append(tlo)  
+
+
+    def _transfrim_trim(self):
+        #trunk or not 
+        for x in self.x_train_file:
+            tmp = [float(ts) for ts in x.split()]
+            if self.dt_type == "feature_time":
+                self.x_train.append(tmp[0:265])
+            elif self.dt_type == "feature_freq":
+                self.x_train.append(tmp[266:])
+            else:
+                self.x_train.append(tmp)
+        for x in self.x_test_file:
+            #x_test.append([float(ts) for ts in x.split()])
+            tmp = [float(ts) for ts in x.split()]
+            if self.dt_type == "feature_time":
+                self.x_test.append(tmp[0:265])
+            elif self.dt_type == "feature_freq":
+                self.x_test.append(tmp[266:])
+            else:
+                self.x_test.append(tmp)
+
+
+
+      
+def _load(dt_type):
+    print("load data...{}".format(dt_type))
+    ds = DataSrc(dt_type)
+    dh = ds.load()
+    print("load data done {}.".format(dt_type))
+    return dh
+
 
 def load_feature():
     return _load("feature")
@@ -27,157 +215,3 @@ def load_raw_acc_z():
 def load_raw_acc_o():
     return _load("raw_acc_o")
 
-def data_path(dat_file):
-    path = None
-    f1 = "data/UCI-HAR-Dataset/"
-    if os.path.isdir(f1):
-        os.path.isfile(f1 + dat_file)
-        path = os.path.abspath(f1 + dat_file)
-    if path is None: 
-        f2 = "../data/UCI-HAR-Dataset/"
-        if os.path.isdir(f2):
-            os.path.isfile(f1 + dat_file)
-            path = os.path.abspath(f2 + dat_file)
-    if path is None:
-        print("Failed find data file for " + dat_file)
-    else:
-        print("data file located {}".format(path))
-    return path        
-
-def _load(dt_type):
-    print("load data...{}".format(dt_type))
-    dt = data()
-
-    # Create empty lists
-    x_train = []
-    y_train = []
-    x_test = []
-    y_test = []
-
-    # Import the HAR dataset
-    if dt_type == "feature" or dt_type == "feature_time" or dt_type == "feature_freq":
-        x_train_file = open(data_path('train/X_train.txt'), 'r')
-        y_train_file = open(data_path('train/y_train.txt'), 'r')
-        x_test_file = open(data_path('test/X_test.txt'), 'r')
-        y_test_file = open(data_path('test/y_test.txt'), 'r')
- 
-    elif dt_type == "raw_acc_x":
-        x_train_file = open(data_path('train/InertialSignals/body_acc_x_train.txt'), 'r')
-        y_train_file = open(data_path('train/y_train.txt'), 'r')
-        x_test_file = open(data_path('test/InertialSignals/body_acc_x_test.txt'), 'r')
-        y_test_file = open(data_path('test/y_test.txt'), 'r')
-
-    elif dt_type == "raw_acc_y":
-        x_train_file = open(data_path('train/InertialSignals/body_acc_y_train.txt'), 'r')
-        y_train_file = open(data_path('train/y_train.txt'), 'r')
-        x_test_file = open(data_path('test/InertialSignals/body_acc_y_test.txt'), 'r')
-        y_test_file = open(data_path('test/y_test.txt'), 'r')
-
-    elif dt_type == "raw_acc_z":
-        x_train_file = open(data_path('train/InertialSignals/body_acc_z_train.txt'), 'r')
-        y_train_file = open(data_path('train/y_train.txt'), 'r')
-        x_test_file = open(data_path('test/InertialSignals/body_acc_z_test.txt'), 'r')
-        y_test_file = open(data_path('test/y_test.txt'), 'r')
-
-    elif dt_type == "raw_acc_o":
-        x_train_file_0 = open(data_path('train/InertialSignals/body_acc_x_train.txt'), 'r')
-        x_train_file_1 = open(data_path('train/InertialSignals/body_acc_y_train.txt'), 'r')
-        x_train_file_2 = open(data_path('train/InertialSignals/body_acc_z_train.txt'), 'r')
-        y_train_file = open(data_path('train/y_train.txt'), 'r')
-        x_test_file_0 = open(data_path('test/InertialSignals/body_acc_x_test.txt'), 'r')
-        x_test_file_1 = open(data_path('test/InertialSignals/body_acc_y_test.txt'), 'r')
-        x_test_file_2 = open(data_path('test/InertialSignals/body_acc_z_test.txt'), 'r')
-        y_test_file = open(data_path('test/y_test.txt'), 'r')      
-
-    # Loop through datasets
-    for y in y_train_file:
-        y_train.append(int(y.rstrip('\n')))
-    for y in y_test_file:
-        y_test.append(int(y.rstrip('\n')))
-    y_test = np.array(y_test)
-    y_train = np.array(y_train)
-
-    if dt_type == "raw_acc_o":
-        tmp0 = []
-        tmp1 = []
-        tmp2 = []
-        for x in x_train_file_0:
-            t0 = [float(ts) for ts in x.split()]
-            tmp0.append(t0)
-        for x in x_train_file_1:
-            t1 = [float(ts) for ts in x.split()]
-            tmp1.append(t1)        
-        for x in x_train_file_2:
-            t2 = [float(ts) for ts in x.split()]
-            tmp2.append(t2)        
-        for i in range(0, len(tmp0)):
-            tlo = [] 
-            tl = tmp0[i]
-            for j in range(0, len(tl)):
-                ox = math.sqrt(tmp0[i][j]*tmp0[i][j] + tmp1[i][j]*tmp1[i][j] + tmp2[i][j]*tmp2[i][j])
-                #if tmp2[i][j] < 0:
-                #    ox = -ox
-                tlo.append(ox)
-            x_train.append(tlo)
-
-        tmp0 = []
-        tmp1 = []
-        tmp2 = []
-        for x in x_test_file_0:
-            t0 = [float(ts) for ts in x.split()]
-            tmp0.append(t0)
-        for x in x_test_file_1:
-            t1 = [float(ts) for ts in x.split()]
-            tmp1.append(t1)        
-        for x in x_test_file_2:
-            t2 = [float(ts) for ts in x.split()]
-            tmp2.append(t2)        
-        for i in range(0, len(tmp0)):
-            tlo = [] 
-            tl = tmp0[i]
-            for j in range(0, len(tl)):
-                ox = math.sqrt(tmp0[i][j]*tmp0[i][j] + tmp1[i][j]*tmp1[i][j] + tmp2[i][j]*tmp2[i][j])
-                #if tmp2[i][j] < 0:
-                #    ox = -ox
-                tlo.append(ox)
-            x_test.append(tlo)  
-    else:
-        for x in x_train_file:
-            tmp = [float(ts) for ts in x.split()]
-            if dt_type == "feature_time":
-                x_train.append(tmp[0:265])
-            elif dt_type == "feature_freq":
-                x_train.append(tmp[266:])
-            else:
-                x_train.append(tmp)
-        for x in x_test_file:
-            #x_test.append([float(ts) for ts in x.split()])
-            tmp = [float(ts) for ts in x.split()]
-            if dt_type == "feature_time":
-                x_test.append(tmp[0:265])
-            elif dt_type == "feature_freq":
-                x_test.append(tmp[266:])
-            else:
-                x_test.append(tmp)
-    # Convert to numpy for efficiency
-    x_train = np.array(x_train)
-    x_test = np.array(x_test)
-
-    print("load data done.")
-    if dt_type == "raw_acc_x" or dt_type == "raw_acc_y" or dt_type == "raw_acc_z" or dt_type == "raw_acc_o":
-        print("normalize data")
-        for i in range(0, len(x_train)):
-            x_train[i] = x_train[i] / np.linalg.norm(x_train[i])
-        for i in range(0, len(x_test)):
-            x_test[i] = x_test[i] / np.linalg.norm(x_test[i])
-
-    # Mapping table for classes
-    labels = {1:'WALKING', 2:'WALKING UPSTAIRS', 3:'WALKING DOWNSTAIRS',
-                4:'SITTING', 5:'STANDING', 6:'LAYING'}
-
-    dt.x_train = x_train
-    dt.y_train = y_train
-    dt.x_test = x_test
-    dt.y_test = y_test
-    dt.labels = labels
-    return dt
