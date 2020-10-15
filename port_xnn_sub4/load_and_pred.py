80046658058b4ce2656cff108b45dd34f52497ef
ethanmac <ethanmac@mac.com>
2020-10-15 16:56:30 +0800

    remove AR1_AR4 (or arCoeff()-1/4) -- the compute process is too complicated even in python

---
+++ port_xnn_sub4/load_and_pred.py
@@ -0,0 +1,75 @@
+import numpy as np
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+
+cpi_filename = "cp_xnn/cp_info.text"
+if not os.path.isfile(cpi_filename):
+    print("Error no cpi_filename {}".format(cpi_filename))
+    sys.exit(-1)
+
+with open(cpi_filename, 'r') as file:
+    test_num = int(file.readline().rstrip())
+    feature_num = int(file.readline().rstrip())
+    skip_ratio = int(file.readline().rstrip())
+    model_name = file.readline()
+
+print("CPI", cpi_filename, test_num, feature_num, skip_ratio, model_name)
+
+import s_data_loader as data_loader
+dt = data_loader.load_feature()
+labels = dt.labels
+y_test = dt.y_test
+ry_test = y_test[::skip_ratio]
+
+
+cpe_filename = "cp_xnn/MLPClassifier.class"
+if not os.path.isfile(cpe_filename):
+    print("Error no cpe_filename {}".format(cpe_filename))
+    sys.exit(-1)
+
+import subprocess
+
+root_dir = os.getcwd()
+working_dir = root_dir + "/cp_xnn"
+os.chdir(working_dir)
+
+quick_skip_ratio = 10
+
+label_raw = []
+for i in range(0, test_num, quick_skip_ratio):
+    tdat = "dat/{}_{:04d}.tdat".format(feature_num, i)
+    with open(tdat, "r") as tdatf:
+        line = tdatf.readline()
+        nums = line.split(" ")
+
+    # ret, stdout = run_command(["java", "MLPClassifier"], nums, cwd=working_dir)
+    ret = True
+
+    cmds = ["java", "MLPClassifier"]
+    cmds += nums 
+    stdout = subprocess.check_output(cmds)
+    print(i, test_num, ret, stdout, ry_test[i]-1)
+    if ret:
+        num_result = stdout.decode("utf-8").strip()
+        if int(num_result) >= 0:
+            label = int(num_result) + 1
+            label_raw.append(label)
+        else:
+            print("error at {} result: {} {}".format(tdat, ret, num_result))
+            label_raw.append(1)
+    else:
+        print("error unknown at {}".foramt(tdat))
+
+os.chdir(root_dir)
+
+ry_pred = np.array(label_raw)
+ry_test = ry_test[::quick_skip_ratio]
+
+from s_confusion import print_confusion_report
+print_confusion_report(ry_pred, ry_test, labels)
+
+from s_confusion import plot_confusion
+plot_confusion(ry_pred, ry_test, labels)
---
+++ port_xnn_sub4/train_and_save.py
@@ -0,0 +1,48 @@
+from sklearn.neural_network import MLPClassifier
+
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+import s_data_loader_sub as data_loader
+# dt = data_loader.load_feature_time()
+dt = data_loader.load_feature_sub4()
+
+# Mapping table for classes
+labels = dt.labels
+x_train = dt.x_train
+y_train = dt.y_train
+x_test = dt.x_test
+y_test = dt.y_test
+
+
+skip_ratio = 1
+rx_train = x_train[::skip_ratio]
+ry_train = y_train[::skip_ratio]
+rx_test = x_test[::skip_ratio]
+ry_test = y_test[::skip_ratio]
+
+max_iter = 800
+tol = 0.0001
+
+model = MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
+       beta_2=0.999, early_stopping=False, epsilon=1e-08,
+       hidden_layer_sizes=(), learning_rate='constant',
+       learning_rate_init=0.001, max_iter=max_iter, momentum=0.9,
+       nesterovs_momentum=True, power_t=0.5, random_state=None,
+       shuffle=True, solver='lbfgs', tol=tol, validation_fraction=0.1,
+       verbose=True, warm_start=False)
+
+model.fit(rx_train, ry_train)
+ry_pred=model.predict(rx_test)
+
+from s_confusion import print_confusion_report
+print_confusion_report(ry_pred, ry_test, labels)
+
+# from s_confusion import plot_confusion
+# plot_confusion(ry_pred, ry_test, labels)
+
+loc_file = __file__
+from s_gen_pred import gen_pred
+gen_pred(model, rx_test, skip_ratio, loc_file)
+print("the training and predict data are all saved in cp_xnn subfolder, run pred to see if java output prediction is ok")
